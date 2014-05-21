fc4317b182d60c988e8d00a32c6a23c99a728fa0
Daniela Baron <baron.daniela@gmail.com>
2014-05-21 18:13:49 -0400

    Initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+rest-tuts
+===================
+
+Learning how to design REST and hypermedia API with TutsPlus.
---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var Datastore = require('nedb');
+var db = {};
+
+// Connect to an NeDB database
+db.movies = new Datastore({ filename: 'db/movies', autoload: true });
+
+// Log every request
+app.use(express.logger('dev'));
+
+// Necessary for accessing POST data via req.body object
+app.use(express.bodyParser());
+
+// Routes
+app.get('/', function (req, res) {
+    res.json(200, {message: 'The API is working.'});
+});
+
+app.listen(process.argv[2] || 3050);
