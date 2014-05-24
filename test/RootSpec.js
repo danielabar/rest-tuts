e609b42010fdfe9631a529b5fe849c551394bc0f
Daniela Baron <baron.daniela@gmail.com>
2014-05-23 20:22:07 -0400

    trying to load test data

--- test/RootSpec.js
+++ test/RootSpec.js
@@ -3,8 +3,20 @@
 var request = require('supertest');
 var assert = require('assert');
 var index = require('../index');
+var testDb = require('../load/TestDb');
 
 describe('Root', function() {
+
+  beforeEach(function(done) {
+    testDb.load(function(err, newDocs) {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        console.log("Inserted " + newDocs.length + " records into test db");
+      }
+      done();
+    });
+  });
 
   describe('GET /', function() {
 
@@ -18,6 +30,18 @@ describe('Root', function() {
         assert.equal("The API is working.", result.message);
         done();
       });
+    });
+
+  });
+
+  describe('GET /movies', function() {
+
+    it('Lists movies', function(done) {
+      request(index)
+      .get('/movies')
+      .expect('Content-Type', /json/)
+      .expect(200);
+      done();
     });
 
   });
